700b1ee5d88a2d20ec51801163e703784a13d92f
Jesuschees <dtrapp@hcrhs.org>
2024-10-24 21:37:51 -0400

    Added IsStopped method, made AtPose independent of velocity. Removed commented out code

--- src/main/include/subsystems/Drivetrain.h
+++ src/main/include/subsystems/Drivetrain.h
@@ -137,8 +137,9 @@ public:
 
   bool AtPose(
     const frc::Pose2d &desiredPose,
-    const frc::Pose2d &tolerance={0.06_m, 0.06_m, 2_deg},
-    const units::meters_per_second_t endVelo = 0.02_mps);
+    const frc::Pose2d &tolerance={0.06_m, 0.06_m, 2_deg});
+
+  bool IsStopped();
 
   // Returns Current Chassis Speed
   frc::ChassisSpeeds GetChassisSpeed();
@@ -176,7 +177,8 @@ public:
       m_field.GetObject("Desired Pose")->SetPose({80_m, 80_m, 0_deg});
     })
     .Until([=, this] {
-      return AtPose(desiredPoseSupplier(), tolerance, endVelo);
+      return AtPose(desiredPoseSupplier(), tolerance) &&
+        IsStopped();
     }
   );
 };
@@ -235,7 +237,7 @@ public:
     {return FollowPathCommand([desiredPose] {return desiredPose;},
      waypoints, endVelo, tolerance);}
 
-    frc2::CommandPtr FollowPathCommand(frc::Trajectory trajectory);
+  frc2::CommandPtr FollowPathCommand(frc::Trajectory trajectory);
   
   /* Constructs a swerve control command from 3 independent controls
    * Each 'cmd' can be one of the following:
