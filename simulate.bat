aa93bef01a6f1f5e7ad3ff34ef0f9bee07704217
bathtubed <ezeward4@gmail.com>
2024-12-24 02:33:12 -0500

    Migrated project to 2025 betas

--- simulate.bat
+++
@@ -1,4 +0,0 @@
-git checkout visvam-wip
-git pull --all
-gradlew simulateExternalNativeDebug   -Dorg.gradle.java.home="C:\Users\Public\wpilib\2024\jdk
-gradlew deploy  -PteamNumber=3637 --offline  -Dorg.gradle.java.home="C:\Users\Public\wpilib\2024\jdk"
